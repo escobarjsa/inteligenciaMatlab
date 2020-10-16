33845061744be25183536153e4c83c08bdb2ff51
JonathanPC <JonathanPC@DESKTOP-VJL0U9R>
2020-10-16 18:51:42 -0500

    ok

--- ejercicio3_carro/ejercicio3_carro.m
+++ ejercicio3_carro/ejercicio3_carro.m
@@ -3,16 +3,16 @@ clear all;
 clc;
 
 %Variables en el plano cartesiano
-x=[0 0 2 2 3 3 6 6 0 0];
-y=[1 2 2 3 3 2 2 1 1 1];
+x=[0 0 2 2 3 3 5 5 0];
+y=[1 2 2 3 3 2 2 1 1];
 
 %Trazar movimiento
 for i=0:10
     
     plot(x,y);
     x=1+x;
-    axis([0,20,0,10])
-    pause(0.01)
-    drawnow()
+    axis([0 20 0 10])
+    pause(0.01) %pausa del carro
+    drawnow()%limita para animar
     
 end
